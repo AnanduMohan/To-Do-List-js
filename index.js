ab05dfdd9fb1bf20251589507e069c7e0eddea2b
AnanduMohan <anandu.tmohan99@gmail.com>
2023-03-14 03:51:51 +0530

    To-Do-List Completion

---
+++ index.js
@@ -0,0 +1,77 @@
+const addForm =document.querySelector(".add");
+const tasks = document.querySelector(".tasks");
+const  clearAll = document.querySelector(".clear");
+const messageSpan = document.querySelector(".message span");
+const searchForm =document.querySelector(".search");
+
+ function updatesMessage(){
+    const textLength = tasks.children.length;
+    messageSpan.textContent = `You have ${textLength} pending tasks.` ;
+ }
+ updatesMessage();
+
+addForm.addEventListener("submit",event =>{
+    event.preventDefault();
+    const value = addForm.task.value.trim();
+
+    if(value.length){
+        tasks.innerHTML += `<li>
+                                <span> ${value} </span>
+                                 <i class="bi bi-trash-fill delete"></i>
+                            </li>`;
+        addForm.reset();
+        updatesMessage();
+    }
+});
+
+tasks.addEventListener("click",event=>{
+  if(event.target.classList.contains("delete")){
+       event.target.parentElement.remove();
+       updatesMessage();
+  }
+
+});
+
+clearAll.addEventListener("click", event=>{
+    const taskItems = tasks.querySelectorAll("li");
+    taskItems.forEach(element => {
+        element.remove();
+        
+    });
+    updatesMessage();
+});
+
+function filterTask(term){
+    Array.from(tasks.children).filter(task=>{
+        return !task.textContent.toLowerCase().includes(term);
+    }).forEach(task=>{
+        task.classList.add("hide");
+    });
+
+    Array.from(tasks.children)
+    .filter(task=>{
+        return task.textContent.toLowerCase().includes(term);
+    }).forEach(task=>{
+        task.classList.remove("hide")
+    });
+}
+
+
+searchForm.addEventListener("keyup", event =>{
+  const term = searchForm.task.value.trim().toLowerCase();
+  filterTask(term);
+});
+
+
+searchForm.addEventListener("click",event=>{
+    if(event.target.classList.contains("reset")){
+        searchForm.reset();
+        const term = searchForm.task.value.trim().toLowerCase();
+        filterTask(term);
+
+    }
+});
+
+
+
+
